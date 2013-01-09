dcb38cf631156b1edc5c6786c61bba424c4c6cbc
Willem de Groot <willem@byte.nl>
2013-01-09 18:22:25 +0100

    added warning if the aggregator is misconfigured

--- lib/carbon/aggregator/receiver.py
+++ lib/carbon/aggregator/receiver.py
@@ -3,7 +3,7 @@ from carbon.aggregator.rules import RuleManager
 from carbon.aggregator.buffers import BufferManager
 from carbon.conf import settings
 from carbon.rewrite import RewriteRuleManager
-from carbon import events
+from carbon import events, log
 
 
 def process(metric, datapoint):
@@ -32,5 +32,6 @@ def process(metric, datapoint):
   for rule in RewriteRuleManager.postRules:
     metric = rule.apply(metric)
 
-  if settings['FORWARD_ALL'] and metric not in aggregate_metrics:
+  if metric not in aggregate_metrics:
+    log.msg("Couldn't match metric %s with any aggregation rule. Passing on un-aggregated." % metric)
     events.metricGenerated(metric, datapoint)
