462b32b68334bcb55808d066aa0ee89903a3e430
Greg DeAngelis <gdeangel@gmail.com>
2013-05-02 21:40:55 -0400

    Reset the reconnection delay on connection
    The reconnection delay should be reset upon success
    to prevent it from continuously growing.

--- lib/carbon/amqp_listener.py
+++ lib/carbon/amqp_listener.py
@@ -173,6 +173,7 @@ class AMQPReconnectingFactory(ReconnectingClientFactory):
         self.verbose = verbose
 
     def buildProtocol(self, addr):
+        self.resetDelay()
         p = self.protocol(self.delegate, self.vhost, self.spec)
         p.factory = self
         return p
