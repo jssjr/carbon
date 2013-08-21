61b6cd708e50029030628ac3ab86585e1844b919
Michael Leinartas <mleinartas@twitter.com>
2013-08-21 07:12:02 -0700

    Merge pull request #132 from huy/0.9.x
    Add new type of cache query - 'cache-query-bulk'

--- lib/carbon/protocols.py
+++ lib/carbon/protocols.py
@@ -152,11 +152,9 @@ class CacheManagementHandler(Int32StringReceiver):
 
       result = dict(datapointsByMetric=datapointsByMetric)
 
-      if settings.LOG_CACHE_HITS:
-        log.query('[%s] cache query bulk for \"%d\" metrics returned %d values' %
-            (self.peerAddr, len(metrics), sum([len(datapoints) for datapoints in datapointsByMetric.values()])))
-      instrumentation.increment('cacheBulkQueries')
-      instrumentation.append('cacheBulkQuerySize', len(metrics))
+      log.query('[%s] cache query bulk for \"%d\" metrics returned %d values' %
+        (self.peerAddr, len(metrics), sum([len(datapoints) for datapoints in datapointsByMetric.values()])))
+      instrumentation.increment('cacheQueries')
 
     elif request['type'] == 'get-metadata':
       result = management.getMetadata(request['metric'], request['key'])
