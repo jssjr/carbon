99de322dd23417bb289a0abe328a674a9709f7a2
Jan Gaedicke <jan.gaedicke@immobilienscout24.de>
2014-01-23 16:24:57 +0100

    check against errno.EEXIST

--- lib/carbon/writer.py
+++ lib/carbon/writer.py
@@ -15,6 +15,7 @@ limitations under the License."""
 import os
 import time
 from os.path import exists, dirname
+import errno
 
 import whisper
 from carbon import state
@@ -123,7 +124,7 @@ def writeCachedDataPoints():
         try:
           os.makedirs(dbDir, 0755)
         except OSError as e:
-          if e.errno != 17:
+          if e.errno != errno.EEXIST:
             log.err("%s" % e)
         log.creates("creating database file %s (archive=%s xff=%s agg=%s)" %
                     (dbFilePath, archiveConfig, xFilesFactor, aggregationMethod))
