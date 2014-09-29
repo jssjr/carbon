f905aa483bc15f848d7b3b5aece07d782a655030
Scott Sanders <jssjr@github.com>
2014-09-29 12:01:50 -0400

    Don't load from lib

--- setup.py
+++ setup.py
@@ -75,7 +75,6 @@ try:
       description='Backend data caching and persistence daemon for Graphite',
       long_description='Backend data caching and persistence daemon for Graphite',
       packages=['carbon', 'carbon.aggregator', 'twisted.plugins'],
-      package_dir={'' : 'lib'},
       scripts=glob('bin/*'),
       package_data={ 'carbon' : ['*.xml'] },
       data_files=install_files,
